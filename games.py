22887e17c693c4561701ab7c8fc2405389736440
SelimGüney <slmgny333@gmail.com>
2024-01-16 17:30:52 +0100

    character limit to answer and character number info at the start

--- games.py
+++ games.py
@@ -1,29 +1,38 @@
-answer = "WORD"
+answer = "ANSWER"
 guess = "_ " * len(answer)
-print(guess)
+print(guess, f"({len(answer)})")
 lives = 5
+wrong_chars = []
 
-def check_letter(answer, lives):
+def check_letter(answer):
     contained = False
-    global guess
+    global guess, lives
 
-    letter = str(input("Guess a letter: ")).upper()
+    
+    while True:
+        letter = str(input("Guess a letter: ")).upper()
+        if len(letter) == 1:
+            break
+        else:
+            print("----- Please enter a single letter! -----")
+        
     for i in range(len(answer)):
         if answer[i] == letter:
             guess = guess[:i*2] + letter + guess[i*2+1:]
             contained = True
     if contained == False:
+
         lives -= 1
         print("You have " + str(lives) + " lives left.")
         if lives == 0:
-            print("You lose!")
+            print("***** You Lost! *****")
             print("The word was " + answer)
             exit()
     print(guess)
 
 while lives > 0:
     if guess.replace(" ", "") != answer:
-        check_letter(answer, lives)
+        check_letter(answer)
     else:
-        print("You win!")
+        print("***** You Win! *****")
         exit()
